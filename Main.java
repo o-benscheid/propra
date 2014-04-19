16dac39a9e1fa4de12d5a2c6e8a9b9fec0565a3b
unknown <oliver.benscheid@hhu.de>
2014-04-19 15:15:52 +0200

    First commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+package de.ollus.intj;
+
+public class Main {
+    public static void halloWelt() {
+        System.out.println("Hallo Welt");
+    }
+
+
+
+    public static void main(String[] args) {
+        halloWelt();
+    }
+}
